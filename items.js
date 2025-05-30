b0bde0fd0c386742538e9b7fc38bd473b3e5adae
galangprihadi <galang.prihadi@gmail.com>
2025-05-31 00:32:04 +0900

    .

---
+++ items.js
@@ -0,0 +1,18 @@
+// =====================================================================  PRODUCTS
+
+const products =[
+    {src: "products/stroller.jpg", id: "KayA"},
+    {src: "products/dompet.jpg", id: "KayB"},
+    {src: "products/penyiram.jpg", id: "KayC"},
+    {src: "products/box1.jpg", id: "KayD"},
+    {src: "products/dot.jpg", id: "KayE"},
+    {src: "products/kandang.jpg", id: "KayF"},
+    {src: "products/mobil.jpg", id: "KayG"},
+    {src: "products/keranjang.jpg", id: "KayH"},
+    {src: "products/kosmetik1.jpg", id: "KayI"},
+    {src: "products/sisir.jpg", id: "KayJ"},
+    {src: "products/panciPink.jpg", id: "KayK"},
+    {src: "products/panPink.jpg", id: "KayL"},
+    {src: "products/closet.jpg", id: "KayM"},
+    {src: "products/panHitam.jpg", id: "KayN"}
+];
